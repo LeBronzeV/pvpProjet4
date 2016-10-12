7e4357ab1731f98bbfd1b3f6964938496ea963e7
LeBronzeV <cacestdelapoubelle@outlook.fr>
2016-10-12 23:41:56 +0200

    4 IA + spawn systeme & début attaque sbire

--- Assets/Scripts/AgentScript.cs
+++ Assets/Scripts/AgentScript.cs
@@ -6,25 +6,24 @@ public class AgentScript : MonoBehaviour
 
     public Transform target;
     public Transform[] targets;
-    public NavMeshAgent[] agent;
-    private int i = 0;
+    public NavMeshAgent agent;
+    [SerializeField]private int i = 0;
 
     // Use this for initialization
-    void Start()
-    {
-        agent[i].SetDestination(target.position);
+    void Start(){
     }
 
     // Update is called once per frame
     void Update()
     {
+        agent.SetDestination(target.position);
         var dist = Vector3.Distance(targets[i].position, transform.position);
         target = targets[i];
-        if (dist < 2)
+        if (dist < 3)
         {
             i++;
             if (i < targets.Length)
-                agent[i].destination = targets[i].position;
+                agent.destination = targets[i].position;
         }
     }
 }
---
+++ Assets/Scripts/attaqueSbireAlly.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class attaqueSbireAlly : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
