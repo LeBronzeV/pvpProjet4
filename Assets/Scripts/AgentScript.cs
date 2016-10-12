23a51017f5b92a5bbc17e39cd63f5e2a5de7a3ac
LeBronzeV <cacestdelapoubelle@outlook.fr>
2016-10-12 09:40:17 +0200

    début

---
+++ Assets/Scripts/AgentScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AgentScript : MonoBehaviour {
+
+    public Transform target;
+    public Transform[] targets;
+    NavMeshAgent agent;
+    private int i = 0;
+
+	// Use this for initialization
+	void Start (){
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(target.position);
+    }
+
+    // Update is called once per frame
+    void Update(){
+        var dist = Vector3.Distance(targets[i].position, transform.position);
+        target = targets[i];
+        if (dist < 2){
+            i++;
+            if (i < targets.Length)
+                agent.destination = targets[i].position;
+            if (i == targets.Length){
+                i = 0;
+                agent.destination = targets[i].position;
+            }
+        }
+    }
+}
