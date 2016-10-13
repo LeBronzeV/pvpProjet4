9af579ac3e51fa9b9b0a49512f3d81142740d28b
LeBronzeV <cacestdelapoubelle@outlook.fr>
2016-10-13 12:00:31 +0200

    IA follow player

---
+++ Assets/Scripts/followPlayer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class followPlayer : MonoBehaviour {
+
+    public NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerStay (Collider other)
+    {
+        if (other.gameObject.layer == 10)
+        {
+            agent.SetDestination(other.gameObject.transform.position);
+        }
+    }
+}
