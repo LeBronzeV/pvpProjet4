e34f3fbe781bad4af090bcb4b7a2ec3a0f1ca06f
pierro1 <pierrick.chevron@sfr.fr>
2016-10-15 02:06:45 +0200

    particule effect

---
+++ Assets/alphas_02_0004s_0004_345/Materials/particule.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class particule : MonoBehaviour {
+    [SerializeField]
+    private ParticleSystem particule1;
+    [SerializeField]
+    private ParticleSystem particule2;
+    [SerializeField]
+    private ParticleSystem particule3;
+    // Use this for initialization
+    void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Z))
+        {
+            particule1.Play();
+            particule2.Play();
+            particule3.Play();
+
+
+        }
+    }
+   
+
+   
+}
